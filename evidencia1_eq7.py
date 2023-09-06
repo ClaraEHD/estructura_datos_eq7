9e029628fccb5f081b4c9d377217a8cbfa2b3eee
Yordan369 <143631867+Yordan369@users.noreply.github.com>
2023-09-06 15:25:43 -0600

    evidencia1_eq7.py

--- evidencia1_eq7.py
+++ evidencia1_eq7.py
@@ -5,7 +5,7 @@ def consultar_por_periodo(notas):
     fecha_inicial =datetime.datetime.strptime(input("Ingrese la fecha inicial dd/mm/aaaa: "),"%d/%m/%Y").date()
     fecha_final =datetime.datetime.strptime(input("Ingrese la fecha final dd/mm/aaaa: "),"%d/%m/%Y").date()
     for nota in notas:
-        if fecha_inicial<=nota['Fecha'] <=fecha_final and nota['Estatus']==False:
+        if fecha_inicial<=nota['Fecha'] <= fecha_final and nota['Estatus']==False:
             info_nota=(f"{nota['Folio']}\t{nota['Fecha']}\t{nota['Cliente']}\t{nota['Monto_pago']}") 
             notas_periodo.append(info_nota)
     if len(notas_periodo)==0:
@@ -30,8 +30,7 @@ def consultar_por_folio(notas):
         else:
             print("No se encontró una nota válida para el folio ingresado.")
 
-def cancelar_nota(notas, notas_canceladas):
-    notas_canceladas=list()
+def cancelar_nota(notas):
     folio = int(input("Ingrese el folio de la nota a cancelar: "))
     for nota in notas:
         if nota['Folio'] == folio and nota['Estatus']==False:
@@ -44,35 +43,36 @@ def cancelar_nota(notas, notas_canceladas):
             confirmacion = input("¿Desea confirmar la cancelación de esta nota? (si/no): ")
             if confirmacion.lower() == 'si':
                 nota['Estatus'] = True
-                notas_canceladas.append(nota)
                 print("Nota cancelada con éxito.")
+                return nota
                 
         else:
             print("No se encontró una nota válida para el folio ingresado.")
 
-def recuperar_nota_cancelada(notas,notas_canceladas):
+def recuperar_nota_cancelada(notas_canceladas):
     print("\nNotas canceladas disponibles:")
     print("Folio\tNombre")
     for nota in notas_canceladas:
-        print(f"{nota['folio']}\t{nota['Cliente']}\n")
+        print(f"{nota['Folio']}\t{nota['Cliente']}\n")
     try:
         folio_recuperar =int(input("Ingrese el folio de la nota cancelada que desea recuperar (o 'no' para cancelar): "))
+        
     except ValueError:
         print("\nVolviendo al menu ")
         return None
     
     for nota in notas_canceladas:
-        if nota['folio'] == folio_recuperar:
+        if nota['Folio'] == folio_recuperar:
             print("\nDetalle de la nota cancelada a recuperar:")
-            print(f"Folio: {nota['folio']}")
-            print(f"Nombre del cliente: {nota['nombre']}")
-            print(f"Tipo de servicio: {nota['servicio']}")
-            print(f"Costo del servicio: {nota['costo']}")
+            print(f"Folio: {nota['Folio']}")
+            print(f"Nombre del cliente: {nota['Cliente']}")
+            print(f"Tipo de servicio: {nota['Detalles']}")
+            print(f"Costo del servicio: {nota['Monto_pago']}")
             confirmacion = input("¿Desea confirmar la recuperación de esta nota cancelada? (s/n): ")
             if confirmacion.lower() == "s" or confirmacion.lower() == "n":
-                print("No pudo proceder la recuperación\nLa nota no fue recuperada")
+                print("La nota fue recuperada con éxito")
             else:
-                return None
+                return nota
     print("No se encontró una nota cancelada válida para el folio ingresado.")
     return None
 
@@ -97,7 +97,7 @@ while True:
         while True:
             try:
                 fecha=datetime.datetime.strptime(input("Ingrese la fecha dd/mm/aaaa: "),"%d/%m/%Y").date()
-                fecha<datetime.datetime.today().date()
+                if fecha<datetime.datetime.today().date()
                 break
             except Exception:
                 print("Ingrese una fecha válida")    
@@ -109,7 +109,7 @@ while True:
         monto_pago=0
         detalle=""
         while True:
-            servicio=input("Ingrese el detalle del servicio realizado: ") #Ingresar más de un servicio
+            servicio=input("Ingrese el detalle del servicio realizado (dejar vacío para cancelar): ") #Ingresar más de un servicio
             if servicio.strip()=="":
                 break
             while True:
@@ -131,7 +131,7 @@ while True:
         nota["Detalles"]=(detalle)
         nota["Estatus"]=(cancelada)
         notas.append(nota)
-        print(nota["Folio"])
+        print(f"El folio es: {nota["Folio"]}")
         for x in notas:
             fechas=x['Fecha']
 
@@ -148,9 +148,10 @@ while True:
         else:
             print("Opción inválida. Por favor, elija una opción válida.")
     elif opcion == "3":
-        cancelar_nota(notas, notas_canceladas)
+        nota_cancelar=cancelar_nota(notas)
+        notas_canceladas.append(nota_cancelar)
     elif opcion == "4":
-        nota_recuperada = recuperar_nota_cancelada(notas,notas_canceladas)
+        nota_recuperada = recuperar_nota_cancelada(notas_canceladas)
         if nota_recuperada:
             notas.append(nota_recuperada)
             print("Nota cancelada recuperada con éxito.")
@@ -161,4 +162,3 @@ while True:
             break
     else:
         print("Opción inválida. Por favor, elija una opción válida.")
-
