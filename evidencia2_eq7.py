7e3ce7d763abc3a139b0c9fc48c6967f12e5dee6
clarahedi <clara.herrerad@uanl.edu.mx>
2023-09-24 15:22:08 -0600

    cambioes menú principal

--- evidencia2_eq7.py
+++ evidencia2_eq7.py
@@ -4,6 +4,125 @@ import re
 import os
 import csv
 
+def menu_principal(notas, notas_canceladas):
+    while True:
+        print("\n--- Menú Taller Mecánico ---")
+        print("1. Registrar una nota")
+        print("2. Consultas y reportes")
+        print("3. Cancelar una nota")
+        print("4. Recuperar una nota")
+        print("5. Salir")
+            
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            nota={}
+            folio_num = len(notas) + 1
+            while True:
+                try:
+                    fecha=datetime.datetime.strptime(input("Ingrese la fecha dd/mm/aaaa: "),"%d/%m/%Y").date()
+                    if fecha<datetime.datetime.today().date():
+                        break
+                    else: 
+                        print("Ingrese una fecha válida")  
+                except ValueError:
+                    print("Ingrese una fecha válida")    
+
+            while True:
+                cliente=input("Ingrese el nombre del cliente: ")
+                if cliente.strip() !="":
+                    break
+                else:
+                    print("No puede dejar el campo vacío")
+    
+            while True:
+                rfc = input("Por favor, ingrese su RFC: ")
+                if validar_rfc(rfc):
+                    break
+                else:
+                    print(f"RFC {rfc} no válido. Ingrese un RFC válido.")
+            
+            monto_pago = 0
+            detalle = ""
+            cant_servicios=0
+            
+            while True:
+                # Pedir al usuario que ingrese un correo
+                correoelectronico = input("Ingresa un correo electrónico: ")
+                if (correoelectronico.strip()==""):
+                    print("El correo no puede ser omitido")
+                elif (validar_correo(correoelectronico)):
+                    break 
+            
+            while True:
+                servicio=input("Ingrese el detalle del servicio realizado (dejar vacío para cancelar): ") #Ingresar mas de un servicio
+                if servicio.strip()=="":
+                    break
+                while True:
+                    try:
+                        costo_servicio=float(input("Ingrese el costo del servicio: "))
+                        if costo_servicio!=0:
+                            break
+                    except ValueError:
+                        print("Ingrese una cantidad valida. ")
+
+                monto_pago+=costo_servicio #suma de los costos del servicio
+                sum_detalle=f"Servicio: {servicio}, Costo: {costo_servicio} \n"
+                detalle+=sum_detalle
+                cant_servicios+=1
+            promedio_monto=monto_pago/cant_servicios
+                
+
+            nota["Folio"] = folio_num
+            nota["Fecha"] = fecha
+            nota["Cliente"] = cliente
+            nota["RFC"] = rfc 
+            nota["Monto_pago"] = monto_pago
+            nota["Detalles"] = detalle
+            nota["Estatus"] = False
+            nota["Promedio_monto"]=promedio_monto
+            notas.append(nota)
+            print(f"El folio es: {nota['Folio']}")
+
+            
+        elif opcion == "2":
+            print("\n--- Submenú Consultas y Reportes ---")
+            print("1. Consulta por período")
+            print("2. Consulta por folio")
+            print("3. Consulta por cliente")
+            print("4. Regresa al menú principal")
+            subopcion = input("Seleccione una opción: ")
+            sub_menu_consultas(subopcion)
+
+        elif opcion == "3":
+            nota_cancelar=cancelar_nota(notas)
+            notas_canceladas.append(nota_cancelar)
+        elif opcion == "4":
+            nota_recuperada = recuperar_nota_cancelada(notas_canceladas)
+            if nota_recuperada:
+                notas.append(nota_recuperada)
+                print("Nota cancelada recuperada con éxito.")
+        elif opcion == "5":
+            confirmacion_salida = input("¿Está seguro de que desea salir? (si/no): ")
+            if confirmacion_salida.lower() == 'si':
+                print("¡Hasta luego!")
+                break
+        else:
+            print("Opción inválida. Por favor, elija una opción válida.")
+
+
+def sub_menu_consultas(opcion):
+            if opcion == "1":
+                consultar_por_periodo(notas)
+            elif opcion == "2":
+                consultar_por_folio(notas)
+            elif opcion == "3":
+                consultar_por_rfc(notas)
+            elif opcion=="4":
+                menu_principal()
+            else:
+                print("Opción inválida. Por favor, elija una opción válida.")
+
+
 ##función para exportar a excel
 def exportar_a_excel(datos, nombre_archivo):
     # Crear un DataFrame a partir de la lista de diccionarios
@@ -115,7 +234,7 @@ def consultar_por_rfc(notas):
             print(f"RFC: {nota['RFC']}\n")
             print(f"Servicio: {nota['Detalles']}\n")
             print(f"Costo del servicio: {nota['Monto_pago']}")
-            print(f"Monto promedio: {nota['Promedio_costo']}")
+            print(f"Monto promedio: {nota['Promedio_monto']}")
             p_importar=input("¿Desea importar la información a excel? S/N")
             if p_importar.upper=="S":
                 nombre_archivo=nota['RFC']+"_"+datetime.datetime.today().date()
@@ -185,107 +304,4 @@ else:
     print("No se ha encontrado un archivo CSV existente.")
     print("Se parte de un estado inicial vacío.")
     
-while True:
-    print("\n--- Menú Taller Mecánico ---")
-    print("1. Registrar una nota")
-    print("2. Consultas y reportes")
-    print("3. Cancelar una nota")
-    print("4. Recuperar una nota")
-    print("5. Salir")
-        
-    opcion = input("Seleccione una opción: ")
-        
-    if opcion == "1":
-        nota={}
-        folio_num = len(notas) + 1
-        while True:
-            try:
-                fecha=datetime.datetime.strptime(input("Ingrese la fecha dd/mm/aaaa: "),"%d/%m/%Y").date()
-                if fecha<datetime.datetime.today().date():
-                    break
-                else: 
-                    print("Ingrese una fecha válida")  
-            except ValueError:
-                print("Ingrese una fecha válida")    
-
-        while True:
-            cliente=input("Ingrese el nombre del cliente: ")
-            if cliente.strip() !="":
-                break
-            else:
-                print("No puede dejar el campo vacío")
-  
-        while True:
-            rfc = input("Por favor, ingrese su RFC: ")
-            if validar_rfc(rfc):
-                break
-            else:
-                print(f"RFC {rfc} no válido. Ingrese un RFC válido.")
-        
-            monto_pago = 0
-            detalle = ""
-        
-        while True:
-            # Pedir al usuario que ingrese un correo
-            correoelectronico = input("Ingresa un correo electrónico: ")
-            if (correoelectronico.strip()==""):
-                print("El correo no puede ser omitido")
-            elif (validar_correo(correoelectronico)):
-                break 
-        
-        while True:
-            servicio=input("Ingrese el detalle del servicio realizado (dejar vacío para cancelar): ") #Ingresar mas de un servicio
-            if servicio.strip()=="":
-                break
-            while True:
-                try:
-                    costo_servicio=float(input("Ingrese el costo del servicio: "))
-                    if costo_servicio!=0:
-                        break
-                except ValueError:
-                    print("Ingrese una cantidad valida. ")
-
-            monto_pago+=costo_servicio #suma de los costos del servicio
-            sum_detalle=f"Servicio: {servicio}, Costo: {costo_servicio} \n"
-            detalle+=sum_detalle
-
-        nota["Folio"] = folio_num
-        nota["Fecha"] = fecha
-        nota["Cliente"] = cliente
-        nota["RFC"] = rfc 
-        nota["Monto_pago"] = monto_pago
-        nota["Detalles"] = detalle
-        nota["Estatus"] = False
-        notas.append(nota)
-        print(f"El folio es: {nota['Folio']}")
-
-        
-    elif opcion == "2":
-        print("\n--- Submenú Consultas y Reportes ---")
-        print("1. Consulta por período")
-        print("2. Consulta por folio")
-        print("3. Consulta por cliente")
-        subopcion = input("Seleccione una opción: ")
-        if subopcion == "1":
-            consultar_por_periodo(notas)
-        elif subopcion == "2":
-            consultar_por_folio(notas)
-        elif subopcion == "3":
-            consultar_por_rfc(notas)
-        else:
-            print("Opción inválida. Por favor, elija una opción válida.")
-    elif opcion == "3":
-        nota_cancelar=cancelar_nota(notas)
-        notas_canceladas.append(nota_cancelar)
-    elif opcion == "4":
-        nota_recuperada = recuperar_nota_cancelada(notas_canceladas)
-        if nota_recuperada:
-            notas.append(nota_recuperada)
-            print("Nota cancelada recuperada con éxito.")
-    elif opcion == "5":
-        confirmacion_salida = input("¿Está seguro de que desea salir? (si/no): ")
-        if confirmacion_salida.lower() == 'si':
-            print("¡Hasta luego!")
-            break
-    else:
-        print("Opción inválida. Por favor, elija una opción válida.")
+menu_principal(notas, notas_canceladas)
